821749e2c593b47cda09f005f624bce97b907f2f
Yoshiyuki Hirano <yhirano@me.com>
2020-03-07 07:31:30 +0900

    Added api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace', 'Api'], function () {
+        Route::post('users/login', 'AuthController@login');
+        Route::post('users', 'AuthController@register');
+
+        Route::get('user', 'UserController@index');
+        Route::match(['put', 'patch'], 'user', 'UserController@update');
+
+        Route::group(['prefix' => 'profiles'], function() {
+            Route::get('{username}', 'ProfileController@show');
+            Route::post('{username}/follow', 'ProfileController@follow');
+            Route::delete('{username}/follow', 'ProfileController@unfollow');
+        });
+
+        Route::resource('articles', 'ArticleController', [
+            'except' => [
+                'create', 'edit'
+            ]
+        ]);
+
+        Route::get('articles/feed', 'ArticleController@feed');
+        Route::post('articles/{article}/favorite', 'ArticleController@favorite');
+        Route::delete('articles/{article}/favorite', 'ArticleController@unFavorite');
+
+        Route::resource('articles/{article}/comments', 'CommentController', [
+            'only' => [
+                'index', 'store', 'destroy'
+            ]
+        ]);
+
+        Route::get('tags', 'TagController@index');
 });
