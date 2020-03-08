7326bf061294c728904e8fb58fd8c6ed8a091602
Yoshiyuki Hirano <yhirano@me.com>
2020-03-08 16:06:05 +0900

    Completed tag section.

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Tag;
-use Illuminate\Http\Request;
+use App\Transformers\TagTransformer;
 
 class TagController extends ApiController
 {
+    public function __construct(TagTransformer $transformer)
+    {
+        $this->transformaer = $transformer;
+    }
+
     public function index()
     {
-        //
+        $tags = Tag::all()->pluck('name');
+        return $this->respondWithTransformer($tags);
     }
 }
